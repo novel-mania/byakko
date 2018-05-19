dc3ea781f500fdf06de0a207f8da9238b75a0010
Rubens Pereira <iorubenspn@gmail.com>
2018-05-19 14:25:07 -0300

    create carrousel and card components

---
+++ components/base/BaseCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <img
+      :src="`img/${info.image}`"
+      alt="Imagem"
+      v-if="hasImage()"
+    >
+    <h2 class="title">{{ info.title }}</h2>
+    <p class="categorie"><i>{{ info.categorie }}</i></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard',
+  props: {
+    info: Object,
+  },
+  methods: {
+    hasImage() {
+      return {}.hasOwnProperty.call(this.info, 'image');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.card {
+  display: inline-block;
+  position: relative;
+  margin: 0 5px;
+
+  > .title {
+    font-size: 1.125rem;
+    color: #333;
+    margin-bottom: 0.1rem;
+    width: 152px;
+    white-space: initial;
+  }
+
+  > .categorie {
+    font-size: 1.125rem;
+    color: #999;
+  }
+}
+</style>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <section class="container">
-  </section>
+  <main class="home">
+    <new-comers />
+  </main>
 </template>
 
 <script>
+import NewComers from '~/components/sections/NewComers';
+
 export default {
   name: 'Home',
+  components: {
+    NewComers,
+  },
 };
 </script>
+
+<style lang="scss">
+
+</style>
+
